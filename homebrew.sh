61ad83bc8e7800f4e2ebf7d9402bf07d11d6390a
jayzone91 <register@j4yz0n3.de>
2024-05-16 18:59:43 +0200

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -7,22 +7,33 @@ echo Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" NONINTERACTIVE=1
 
 echo adding brew to the zsh path
-echo -n 'export PATH=/opt/homebrew/bin/' >> ~/.zshrc
-
-echo source new path
-source ~/.zshrc
+echo (eval "$(/opt/homebrew/bin/brew shellenv)") >> ~/.zprofile
+echo "TODO: Hier muss ich noch mal ran! Das passt noch nicht! Da fehlt noch was."
 
 echo install Formulaes
-brew install --formulae fzf go luarocks nvm ripgrep powerlevel10k ruby zig zoxide zsh-syntax-highlighting
-
-echo install Neovim Nightly
-brew install --formulae neovim --HEAD
+brew install --formulae bat cmake composer curl eza fd fzf go lua luajit luarocks mas neovim ninja nvm php powerlevel10k python ripgrep thefuck yt-dlp zig zoxide zsh-autosuggestions zsh-syntax-highlighting
 
 echo install Casks
-brew install alfred appcleaner betterdisplay cheatsheet github iterm2@nightly linearmouse
+brew install --cask alacritty alt-tab font-meslo-for-powerlevel10k appcleaner betterdisplay cheatsheet github linearmouse arc steam anydesk command-x raycast rectangle
 
-echo Write zshconfig
-echo -n 'if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]] then' >> ~/.zshrc
-echo -n 'source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh' >> ~/.zshrc
-echo -n 'fi' >> ~/.zshrc
+echo write .zsrc file and rename old one
+mv ~/.zshrc ~/.zshrc.old
+curl https://raw.githubusercontent.com/jayzone91/dotfiles/main/.zshrc -o ~/.zshrc
 
+echo write powerlevel10k file
+curl https://raw.githubusercontent.com/jayzone91/dotfiles/main/.p10k.zsh -o ~/.p10k.zsh
+
+echo write alacritty config
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+curl https://raw.githubusercontent.com/jayzone91/dotfiles/main/.config/alacritty/alacritty.toml -o ~/.config/alacritty/alacritty.toml
+
+echo write neovim config
+git clone https://github.com/jayzone91/nvim ~/.config/nvim/
+
+echo source zsrc
+source ~/.zshrc
+
+echo install node lts
+nvm install --lts
+nvm use --lts
